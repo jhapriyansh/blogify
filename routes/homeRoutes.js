1e76164e6d5895ee07810c83ebd0fa5df453a3a0
PRIYANSHU KUMAR JHA <priyanshukumarjha@Priyanshus-MacBook-Air.local>
2025-07-25 00:27:09 +0530

    initial commit, backend features added, register, login, add post, remove post, get all posts, home feed

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,6 +3,8 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const { mainFeedController } = require("../controllers/homeController");
 const router = express.Router();
 
+// comment, like features will be added in this
+
 router.post("/feed", authMiddleware, mainFeedController);
 
 module.exports = router;
